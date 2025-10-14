b7b57ca605e4dfc8d991bfa975898eb0d367af0f
Osvaldo Frias <osvaldo.frias@issivs.com>
2025-10-14 10:27:21 -0600

    feat: enhance Pokémon game functionality with loading state and Pokémon interface

--- src/modules/pokemon/composables/usePokemonGame.ts
+++ src/modules/pokemon/composables/usePokemonGame.ts
@@ -1,20 +1,42 @@
-import { onMounted, ref } from "vue";
-import { GameStatus, type PokemonListResponse } from "../interfaces";
+import { computed, onMounted, ref } from "vue";
+import { GameStatus, type Pokemon, type PokemonListResponse } from "../interfaces";
 import { pokemonApi } from "../api/pokemonApi";
 
 export const usePokemonGame = () => {
   const gameStatus = ref<GameStatus>(GameStatus.Playing);
+  const pokemons = ref<Pokemon[]>([]);
+  const pokemonOptions = ref<Pokemon[]>([]);
 
-  const getPokemons = async () => {
+  const isLoading = computed(() => pokemons.value.length === 0);
+
+  const getPokemons = async (): Promise<Pokemon[]> => {
     const response = await pokemonApi.get<PokemonListResponse>('/?limit=151');
-    console.log(response.data);
+    const pokemonArray = response.data.results.map(pokemon => {
+      const urlParts = pokemon.url.split('/');
+      const id = urlParts[urlParts.length - 2] ?? 0;
+      return {
+        id: +id,
+        name: pokemon.name,
+      };
+    });
+    return pokemonArray.sort(() => Math.random() - 0.5);
   };
 
-  onMounted(() => {
-    getPokemons();
+  const getNextOptions = (howMany: number = 4) => {
+    gameStatus.value = GameStatus.Playing;
+    pokemonOptions.value = pokemons.value.slice(0, howMany);
+    pokemons.value = pokemons.value.slice(howMany);
+  }
+
+  onMounted(async () => {
+    pokemons.value = await getPokemons();
+    // console.log(pokemons.value);
+    getNextOptions();
+    console.log(pokemonOptions.value);
   });
 
   return {
     gameStatus,
+    isLoading,
   };
 }
--- src/modules/pokemon/pages/PokemonGame.vue
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -1,9 +1,9 @@
 <template>
-  <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+  <section v-if="isLoading" class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="text-3xl">Espere por favor...</h1>
     <h3 class="animate-pulse">Cargando Pokémon</h3>
   </section>
-  <section class="flex flex-col justify-center items-center w-screen h-screen">
+  <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="m-5">¿Cuál es este Pokémon?</h1>
     <!-- Pokémon Picture -->
     <PokemonPicture />
@@ -17,5 +17,5 @@ import PokemonOptions from '../components/PokemonOptions.vue';
 import PokemonPicture from '../components/PokemonPicture.vue';
 import { usePokemonGame } from '../composables/usePokemonGame';
 
-const { gameStatus } = usePokemonGame();
+const { gameStatus, isLoading } = usePokemonGame();
 </script>
