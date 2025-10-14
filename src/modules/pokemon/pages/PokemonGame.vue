436bd1b23b9af14100268fc0be21138de2846bd7
Osvaldo Frias <osvaldo.frias@issivs.com>
2025-10-14 10:10:47 -0600

    feat: initialize Pokémon game project with Vue 3 and TypeScript
    - Added main application structure with App.vue and routing.
    - Implemented Pokémon game components including Pokémon options and picture display.
    - Integrated Pokémon API using Axios for fetching data.
    - Styled application with Tailwind CSS and custom animations.
    - Set up TypeScript configuration for the project.

---
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -0,0 +1,21 @@
+<template>
+  <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="text-3xl">Espere por favor...</h1>
+    <h3 class="animate-pulse">Cargando Pokémon</h3>
+  </section>
+  <section class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="m-5">¿Cuál es este Pokémon?</h1>
+    <!-- Pokémon Picture -->
+    <PokemonPicture />
+    <!-- Pokémon Options -->
+    <PokemonOptions />
+  </section>
+</template>
+
+<script setup lang="ts">
+import PokemonOptions from '../components/PokemonOptions.vue';
+import PokemonPicture from '../components/PokemonPicture.vue';
+import { usePokemonGame } from '../composables/usePokemonGame';
+
+const { gameStatus } = usePokemonGame();
+</script>
